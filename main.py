4c8c574b67946f18d9ae6d98d55fb16e4bb8b2af
blinchiksmyasom <blinchiksmyasom@protonmail.com>
2018-01-25 18:53:35 +0300

    Move features to dictionary. Write simple test for LIDC and NSCLC.

--- main.py
+++ main.py
@@ -7,11 +7,22 @@
 from util import loader, featureExtractor
 
 if __name__ == '__main__':
-    lidc_data = loader.load_nodules("lidc-data/", "LIDC")
+    lidc_data = loader.load_nodules('lidc-data/', 'LIDC')
+    nsclc_data = loader.load_nodules('nsclc-data/', 'NSCLC')
 
     lidc_features = featureExtractor.get_features(lidc_data[:5])
+    nsclc_features = featureExtractor.get_features(nsclc_data)
 
     print(lidc_features)
+    print(nsclc_features)
+
+    print("LIDC:")
+    for f in lidc_features:
+        print (f)
+
+    print("NSCLC:")
+    for f in nsclc_features:
+        print (f)
 
     # plt.imshow(i, cmap = cm.Greys_r)
     # plt.show()
--- util/featureExtractor.py
+++ util/featureExtractor.py
@@ -10,18 +10,16 @@ def get_feature(comatrix, mode):
 class Features:
     def __init__(self, source_id):
         self.source_id = source_id
-
         self.max_coord = 0
-        self.grey_comatrix = []
+        self.features = dict()
 
-        #
-        self.contrast = []
-        self.dissimilarity = []
-        self.homogeneity = []
-        self.energy = []
-        self.correlation = []
-        self.ASM = []
-        self.ent = 0
+    def __str__(self):
+        string = 'Features for ' + self.source_id
+
+        for key, value in self.features.iteritems():
+            string += '\n  ' + key + ' = ' + str(value)
+
+        return string
 
 
 def get_features(lidc_data):
@@ -35,15 +33,13 @@ def get_features(lidc_data):
         grey_comatrix = skimg.greycomatrix(nodule.pixels, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4],
                                            nodule_feature.max_coord + 1)
 
-        #nodule_feature.grey_comatrix = grey_comatrix
-
-        nodule_feature.contrast = skimg.greycoprops(grey_comatrix, 'contrast')
-        nodule_feature.dissimilarity = skimg.greycoprops(grey_comatrix, 'dissimilarity')
-        nodule_feature.homogeneity = skimg.greycoprops(grey_comatrix, 'homogeneity')
-        nodule_feature.energy = skimg.greycoprops(grey_comatrix, 'energy')
-        nodule_feature.correlation = skimg.greycoprops(grey_comatrix, 'correlation')
-        nodule_feature.ASM = skimg.greycoprops(grey_comatrix, 'ASM')
-        nodule_feature.ent = entropy(nodule.pixels)
+        nodule_feature.features['contrast'] = skimg.greycoprops(grey_comatrix, 'contrast')
+        nodule_feature.features['dissimilarity'] = skimg.greycoprops(grey_comatrix, 'dissimilarity')
+        nodule_feature.features['homogeneity'] = skimg.greycoprops(grey_comatrix, 'homogeneity')
+        nodule_feature.features['energy'] = skimg.greycoprops(grey_comatrix, 'energy')
+        nodule_feature.features['correlation'] = skimg.greycoprops(grey_comatrix, 'correlation')
+        nodule_feature.features['ASM'] = skimg.greycoprops(grey_comatrix, 'ASM')
+        nodule_feature.features['entropy'] = entropy(nodule.pixels)
 
         features.append(nodule_feature)
 
