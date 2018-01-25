0f0f5df13e6f45cc72dfdff22b566d6c700dc5ed
blinchiksmyasom <blinchiksmyasom@protonmail.com>
2018-01-25 20:05:52 +0300

    Rename module

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib.cm as cm
 import numpy as np
 from sklearn.svm import SVC
 
-from util import loader, featureExtractor
+from util import loader, feature_extractor
 
 
 def show_nodule(nodule):
@@ -15,8 +15,8 @@ if __name__ == '__main__':
     lidc_data = loader.load_nodules('lidc-data/', 'LIDC')
     nsclc_data = loader.load_nodules('nsclc-data/', 'NSCLC')
 
-    lidc_features = featureExtractor.get_features(lidc_data[:10])
-    nsclc_features = featureExtractor.get_features(nsclc_data)
+    lidc_features = feature_extractor.get_features(lidc_data[:10])
+    nsclc_features = feature_extractor.get_features(nsclc_data)
 
     print("LIDC:")
     for f in lidc_features:
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     for f in nsclc_features:
         print (f)
 
-    X = featureExtractor.features_to_matrix(lidc_features)
+    X = feature_extractor.features_to_matrix(lidc_features)
     y = np.random.choice(a=[False, True], size=len(lidc_features))  # TODO
 
     svm = SVC()
