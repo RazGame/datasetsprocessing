21c8c8ecd45769e8318f9ffbcc20fde6bada2a07
blinchiksmyasom <blinchiksmyasom@protonmail.com>
2018-01-25 19:07:57 +0300

    Flatten some features

--- util/featureExtractor.py
+++ util/featureExtractor.py
@@ -36,12 +36,12 @@ def get_features(lidc_data):
             grey_comatrix = skimg.greycomatrix(nodule.pixels, [1], [0, np.pi / 4, np.pi / 2, 3 * np.pi / 4],
                                                nodule_feature.max_coord + 1)
 
-        nodule_feature.features['contrast'] = skimg.greycoprops(grey_comatrix, 'contrast')
-        nodule_feature.features['dissimilarity'] = skimg.greycoprops(grey_comatrix, 'dissimilarity')
-        nodule_feature.features['homogeneity'] = skimg.greycoprops(grey_comatrix, 'homogeneity')
-        nodule_feature.features['energy'] = skimg.greycoprops(grey_comatrix, 'energy')
-        nodule_feature.features['correlation'] = skimg.greycoprops(grey_comatrix, 'correlation')
-        nodule_feature.features['ASM'] = skimg.greycoprops(grey_comatrix, 'ASM')
+        nodule_feature.features['contrast'] = skimg.greycoprops(grey_comatrix, 'contrast').flatten()
+        nodule_feature.features['dissimilarity'] = skimg.greycoprops(grey_comatrix, 'dissimilarity').flatten()
+        nodule_feature.features['homogeneity'] = skimg.greycoprops(grey_comatrix, 'homogeneity').flatten()
+        nodule_feature.features['energy'] = skimg.greycoprops(grey_comatrix, 'energy').flatten()
+        nodule_feature.features['correlation'] = skimg.greycoprops(grey_comatrix, 'correlation').flatten()
+        nodule_feature.features['ASM'] = skimg.greycoprops(grey_comatrix, 'ASM').flatten()
         nodule_feature.features['entropy'] = entropy(nodule.pixels)
 
         features.append(nodule_feature)
